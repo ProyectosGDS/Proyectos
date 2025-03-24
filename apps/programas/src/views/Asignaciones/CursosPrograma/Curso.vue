6f1d483e1c4e9fd0728a780cdd60520ca2a58ad8
Nelson Vasquez <gds.proyectos2024.gt@gmail.com>
2025-03-24 16:05:08 -0600

    Se corrige palabra inpulsatec por impulsatec

--- apps/programas/src/views/Asignaciones/CursosPrograma/Curso.vue
+++ apps/programas/src/views/Asignaciones/CursosPrograma/Curso.vue
@@ -19,7 +19,7 @@
     <Input v-model="cursos.curso.nombre" option="label" title="*Nombre" maxlength="80" :error="cursos.errors.hasOwnProperty('nombre')" />
     <Input v-model="cursos.curso.descripcion" option="text-area" title="Descripción" placeholder="Describe el curso ..." rows="3" maxlength="500" :error="cursos.errors.hasOwnProperty('descripcion')" />
     <div class="flex justify-evenly uppercase text-color-4">
-                <label class="uppercase">*Inpulsatec</label>
+                <label class="uppercase">*Impulsatec</label>
                 <div class="flex items-center gap-2">
                     <span class="text-sm text-gray-500">Sí</span>
                     <Switch v-model="store.curso.inpulsatec" class="h-auto w-14 bg-red-400 has-[:checked]:bg-green-500" :values="['S','N']" />
