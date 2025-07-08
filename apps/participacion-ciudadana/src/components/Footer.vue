85ce22037277c7c9b2cbb0cf0cfe443d3261a5d1
Nelson Vasquez <gds.proyectos2024.gt@gmail.com>
2025-07-08 12:51:44 -0600

    se modifica url de la imagenes del footer

--- apps/participacion-ciudadana/src/components/Footer.vue
+++ apps/participacion-ciudadana/src/components/Footer.vue
@@ -3,35 +3,20 @@ import { ref } from 'vue';
 
     
     const images = ref([
-        { alt : 'Municipalidad de guatemala', url : '../assets/img/icons-footer/1.png',link : 'https://www.muniguate.com' },
-        { alt : 'Municipalidad de guatemala', url : '../assets/img/icons-footer/2.png',link : '#' },
-        { alt : 'Municipalidad de guatemala', url : '../assets/img/icons-footer/3.png',link : '#' },
-        { alt : 'Municipalidad de guatemala', url : '../assets/img/icons-footer/4.png',link : '#' },
-        { alt : 'Municipalidad de guatemala', url : '../assets/img/icons-footer/5.png',link : '#' },
-        { alt : 'Municipalidad de guatemala', url : '../assets/img/icons-footer/6.png',link : '#' },
+        { alt : 'Municipalidad de guatemala', url : '/participacion-ciudadana/img/icons-footer/1.png',link : 'https://www.muniguate.com' },
+        { alt : 'Municipalidad de guatemala', url : '/participacion-ciudadana/img/icons-footer/2.png',link : '#' },
+        { alt : 'Municipalidad de guatemala', url : '/participacion-ciudadana/img/icons-footer/3.png',link : '#' },
+        { alt : 'Municipalidad de guatemala', url : '/participacion-ciudadana/img/icons-footer/4.png',link : '#' },
+        { alt : 'Municipalidad de guatemala', url : '/participacion-ciudadana/img/icons-footer/5.png',link : '#' },
+        { alt : 'Municipalidad de guatemala', url : '/participacion-ciudadana/img/icons-footer/6.png',link : '#' },
     ])
 
 </script>
 <template>
     <footer class="flex flex-col gap-y-8 justify-center items-center bg-color-8 h-64">
         <div class="flex gap-x-5 lg:gap-x-16">
-            <a href="https://www.muniguate.com">
-                <img src="../assets/img/icons-footer/1.png" alt="Municipalidad de Guatemala" class="md:h-16 h-10"> 
-            </a>
-            <a href="#">
-                <img src="../assets/img/icons-footer/2.png" alt="Municipalidad de Guatemala" class="md:h-16 h-10"> 
-            </a>
-            <a href="#">
-                <img src="../assets/img/icons-footer/3.png" alt="Municipalidad de Guatemala" class="md:h-16 h-10"> 
-            </a>
-            <a href="#">
-                <img src="../assets/img/icons-footer/4.png" alt="Municipalidad de Guatemala" class="md:h-16 h-10"> 
-            </a>
-            <a href="#">
-                <img src="../assets/img/icons-footer/5.png" alt="Municipalidad de Guatemala" class="md:h-16 h-10"> 
-            </a>
-            <a href="#">
-                <img src="../assets/img/icons-footer/6.png" alt="Municipalidad de Guatemala" class="md:h-16 h-10"> 
+            <a v-for="image in images" :href="image.link">
+                <img :src="image.url" :alt="image.alt" class="md:h-16 h-10"> 
             </a>
         </div>
         <div>
